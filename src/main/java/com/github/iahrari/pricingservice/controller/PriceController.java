009f727b7a0f97741d15c6111a7d59ba8efa0a3e
Iman Ahrari <iahrari@pm.me>
2021-12-26 21:21:47 +0330

    refactor(controller): change get to post

--- src/main/java/com/github/iahrari/pricingservice/controller/PriceController.java
+++ src/main/java/com/github/iahrari/pricingservice/controller/PriceController.java
@@ -1,70 +1,28 @@
 package com.github.iahrari.pricingservice.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import javax.validation.Valid;
 
-import com.github.iahrari.pricingservice.controller.exception.QueryException;
-import com.github.iahrari.pricingservice.domain.Price;
-import com.github.iahrari.pricingservice.domain.QueryError;
+import com.github.iahrari.pricingservice.dto.Price;
+import com.github.iahrari.pricingservice.service.PriceService;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/price")
+@RequestMapping("/api/v1/price")
 public class PriceController {
+    private final PriceService priceService;
 
-    private final List<String> addresses = Arrays.asList("Vanak", "Azadi", "Tehran Pars", "Shohada");
+    public PriceController(PriceService priceService) {
+        this.priceService = priceService;
+    }
     
-    @GetMapping
-    public Price getPrice(@RequestParam String source, @RequestParam String destination) {
-        if(ifAnyEquals(source, destination, addresses.get(0)) 
-                    && ifAnyEquals(source, destination, addresses.get(1))) {
-            return new Price(source, destination, 1500d);
-        } else if(ifAnyEquals(source, destination, addresses.get(0)) 
-                    && ifAnyEquals(source, destination, addresses.get(2))) {
-            return new Price(source, destination, 2000d);
-        } else if(ifAnyEquals(source, destination, addresses.get(1)) 
-                    && ifAnyEquals(source, destination, addresses.get(2))) {
-            return new Price(source, destination, 1000d);
-        } else if(ifAnyEquals(source, destination, addresses.get(2)) 
-                    && ifAnyEquals(source, destination, addresses.get(3))) {
-            return new Price(source, destination, 1900d);
-        } else if(ifAnyEquals(source, destination, addresses.get(3)) 
-                    && ifAnyEquals(source, destination, addresses.get(1))) {
-            return new Price(source, destination, 2600d);
-        } else if(ifAnyEquals(source, destination, addresses.get(3)) 
-                    && ifAnyEquals(source, destination, addresses.get(0))) {
-            return new Price(source, destination, 3000d);
-        } else {
-            throw checkWrongFields(source, destination);
-        } 
-    }
-
-    private boolean ifAnyEquals(String source, String destination, String target) {
-        return source.equalsIgnoreCase(target) || destination.equalsIgnoreCase(target);
-    }
-
-    private QueryException checkWrongFields(String source, String destination) {
-        List<String> fields = new ArrayList<>();
-            List<String> values = new ArrayList<>();
-            var isSourceValid = addresses.stream()
-                .allMatch(address -> !source.equalsIgnoreCase(address));
-            if(isSourceValid) {
-                fields.add("source");
-                values.add(source);
-            }
-
-            var isDestinationValid = addresses.stream()
-                .allMatch(address -> !destination.equalsIgnoreCase(address));
-
-            if(isDestinationValid) {
-                fields.add("destination");
-                values.add(destination);
-            }
-            return new QueryException(new QueryError(fields, values));
-    }
+    @PostMapping
+    public ResponseEntity<Price> getPrice(@Valid @RequestBody Price price) {
+        return ResponseEntity.ok(priceService.getPrice(
+            price.getSource(), price.getDestination()));
+    } 
 }
